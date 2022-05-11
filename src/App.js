134d40c0d91e4e8a005365b75c437324fb696cfc
sharkz <sharkz@localhost.localdomain>
2022-05-11 12:52:20 -0400

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Draggable from "react-draggable";
+import { MapInteractionCSS } from 'react-map-interaction';
+
+import Modal from './componets/Modal';
+
 
 function App() {
+  const [show, setShow] = useState(false);
+  const [text, setText] = useState('')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='castle-overlay overlay'>
+      {/* <Draggable
+      defaultPosition={{x: 0, y: 0}}
+      > */}
+        <MapInteractionCSS>
+    <div className="castle-bg" draggable="false" > 
+
+     <div className='fire-wrap-1 fire-wrap'>
+       <div className='fire'></div>
+     </div>
+     
+     <div className='quest-btn click-cursor'>
+       <button className='game-button click-cursor' onClick={()=>{setShow(true); setText('Quest')}}>
+         <div className='title'>Quest</div>
+         <img src='../scr/assets/bubble-arrow.c34d2c3a.png' alt='buble'/>
+       </button>
+     </div>
+
+     <div className='jester-container'>
+       <div className='jester-wrap'> 
+     <div className='jester-grandle-btn click-cursor'>
+       <button className='game-button click-cursor' onClick={()=>{setShow(true); setText('Stake')}}>
+         <div className='title'>Stake</div>
+         <img src='../scr/assets/bubble-arrow.c34d2c3a.png' alt='buble'/>
+       </button>
+     </div>
+     </div>
+     </div>
+
+     <div className='jester-container'>
+       <div className='jester-wrap'> 
+     <div className='clp-grandle-btn click-cursor'>
+       <button className='game-button click-cursor' onClick={()=>{setShow(true); setText('CLP')}}>
+         <div className='title'>CLP</div>
+         <img src='../scr/assets/bubble-arrow.c34d2c3a.png' alt='buble'/>
+       </button>
+     </div>
+     </div>
+     </div>
+
+     <div className='land-auction-btn click-cursor'>
+       <button className='game-button click-cursor' onClick={()=>{setShow(true); setText('Market Place')}}>
+         <div className='title'>Market Place</div>
+         <img src='../scr/assets/bubble-arrow.c34d2c3a.png' alt='buble'/>
+       </button>
+     </div>
+
+     <div className='sum-btn click-cursor' onClick={()=>{setShow(true); setText('Summons')}}>
+       <button className='game-button click-cursor'>
+         <div className='title'> Summons</div>
+         <img src='../scr/assets/bubble-arrow.c34d2c3a.png' alt='buble'/>
+       </button>
+     </div>
+
+
+
+     <Modal show={show} setShow={setShow} text={text}/>
+
+    </div>
+    </MapInteractionCSS>
+    {/* </Draggable> */}
     </div>
   );
 }
---
+++ src/componets/Modal.js
@@ -0,0 +1,20 @@
+function Modal({show,setShow,text}) {
+    return (
+      <div className={show ? `modal-overlay overlay active` : `modal-overlay overlay`}>      
+         <div className="modal game-border fancy" style={{width: '100vh',height: '264px',marginBottom: '30px'}}>
+             <div className="close-btn click-cursor" onClick={()=>setShow(false)}></div>
+             <h3 className="modal-title fancy">
+                 <span>{text}</span>
+             </h3>
+             <div className="modal-body"></div>
+             <div className="coming-soon__wrap">
+                 <div className="coming-soon">
+                     <span>Comming Soon</span>
+                 </div>
+             </div>
+         </div>
+      </div>
+    );
+  } 
+  
+  export default Modal;
